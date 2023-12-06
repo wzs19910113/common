e316dff553870d21b116727e892ffdba92f5a99d
wzs19910113 <wzs19910113@hotmail.com>
2023-12-06 09:57:02 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/Home'
-import Start from '@/pages/Start'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [
         {
-            path: '/home',
+            path: '/',
             name: 'Home',
             component: Home
         },
-        {
-            path: '/',
-            name: 'Start',
-            component: Start
-        }
     ]
 })
